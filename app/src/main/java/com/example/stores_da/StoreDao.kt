c87184ed9826b0565e422172008e8ff170692ca1
Josue <sjosue5082002@gmail.com>
2021-11-30 21:14:44 -0600

    DAO

---
+++ app/src/main/java/com/example/stores_da/StoreDao.kt
@@ -0,0 +1,18 @@
+package com.example.stores_da
+
+import androidx.room.*
+
+@Dao
+interface StoreDao {
+    @Query("SELECT * FROM StoreEntity")
+    fun getAllStores() : MutableList<StoreEntity>
+
+    @Insert
+    fun addStore(storeEntity: StoreEntity)
+
+    @Update
+    fun updateStore(storeEntity: StoreEntity)
+
+    @Delete
+    fun deleteStore(storeEntity: StoreEntity)
+}
