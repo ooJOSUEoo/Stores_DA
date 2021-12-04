0e455e2c395f3b6f9ddaafb9c68cc73af0ea3b9c
Josue <sjosue5082002@gmail.com>
2021-12-03 19:14:38 -0600

    Ocultar el teclado

--- app/src/main/java/com/example/stores_da/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores_da/EditStoreFragment.kt
@@ -1,7 +1,9 @@
 package com.example.stores_da
 
+import android.content.Context
 import android.os.Bundle
 import android.view.*
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.example.stores_da.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
@@ -50,17 +52,31 @@ class EditStoreFragment : Fragment() {
                 doAsync {
                     StoreApplication.database.storeDao().addStore(store)
                     uiThread {
+                        hideKeyboard()
+
                         Snackbar.make(mBinding.root,
                             getString(R.string.edit_store_message_save_success),
                             Snackbar.LENGTH_SHORT)
                             .show()
+                         mActivity?.onBackPressed()
                     }
                 }
                 true
             }
             else -> super.onOptionsItemSelected(item)
         }
-        //return super.onOptionsItemSelected(item)
+    }
+
+    private fun hideKeyboard(){
+        val imm = mActivity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (view != null){
+            imm.hideSoftInputFromWindow(view!!.windowToken,0)
+        }
+    }
+
+    override fun onDestroyView() {
+        hideKeyboard()
+        super.onDestroyView()
     }
 
     override fun onDestroy() {
