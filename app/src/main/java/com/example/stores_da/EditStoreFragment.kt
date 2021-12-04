36ad9a59b4fb8511277e91d1e081a30065109545
Josue <sjosue5082002@gmail.com>
2021-12-03 18:37:55 -0600

    Insertar registro desde Fragment

--- app/src/main/java/com/example/stores_da/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores_da/EditStoreFragment.kt
@@ -5,6 +5,8 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import com.example.stores_da.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 
 class EditStoreFragment : Fragment() {
@@ -41,10 +43,19 @@ class EditStoreFragment : Fragment() {
                 true
             }
             R.id.action_save -> {
-                Snackbar.make(mBinding.root,
-                    getString(R.string.edit_store_message_save_success),
-                    Snackbar.LENGTH_SHORT)
-                    .show()
+                val store = StoreEntity(name = mBinding.etName.text.toString().trim(),
+                    phone = mBinding.etPhone.text.toString().trim(),
+                    website = mBinding.etWebsite.text.toString().trim())
+
+                doAsync {
+                    StoreApplication.database.storeDao().addStore(store)
+                    uiThread {
+                        Snackbar.make(mBinding.root,
+                            getString(R.string.edit_store_message_save_success),
+                            Snackbar.LENGTH_SHORT)
+                            .show()
+                    }
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
