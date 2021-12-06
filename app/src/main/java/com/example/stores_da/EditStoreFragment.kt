d0617b0637fe67be69231f9528a387ee27cc0cac
Josue <sjosue5082002@gmail.com>
2021-12-06 17:58:55 -0600

    Validar TextFields: Foco

--- app/src/main/java/com/example/stores_da/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores_da/EditStoreFragment.kt
@@ -87,7 +87,7 @@ class EditStoreFragment : Fragment() {
                 true
             }
             R.id.action_save -> {
-                if (mStoreEntity != null){
+                if (mStoreEntity != null && validateFields()){
                     /*val store = StoreEntity(name = mBinding.etName.text.toString().trim(),
                     phone = mBinding.etPhone.text.toString().trim(),
                     website = mBinding.etWebsite.text.toString().trim(),
@@ -126,6 +126,28 @@ class EditStoreFragment : Fragment() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun validateFields(): Boolean {
+        var isValid = true
+
+        if (mBinding.etPhotoUrl.text.toString().trim().isEmpty()){
+            mBinding.tilPhotoUrl.error = getString(R.string.helper_required)
+            mBinding.etPhotoUrl.requestFocus()
+            isValid = false
+        }
+        if (mBinding.etPhone.text.toString().trim().isEmpty()){
+            mBinding.tilPhone.error = getString(R.string.helper_required)
+            mBinding.etPhone.requestFocus()
+            isValid = false
+        }
+        if (mBinding.etName.text.toString().trim().isEmpty()){
+            mBinding.tilName.error = getString(R.string.helper_required)
+            mBinding.etName.requestFocus()
+            isValid = false
+        }
+
+        return isValid
     }
 
     private fun hideKeyboard(){
