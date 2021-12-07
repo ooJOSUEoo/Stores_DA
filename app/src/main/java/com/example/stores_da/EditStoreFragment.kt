c98f784440caa7c198561a1e54d01567c6c85fb0
Josue <sjosue5082002@gmail.com>
2021-12-06 18:55:22 -0600

    Ultimas configuraciones a los TextFields

--- app/src/main/java/com/example/stores_da/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores_da/EditStoreFragment.kt
@@ -50,16 +50,26 @@ class EditStoreFragment : Fragment() {
 
         setHasOptionsMenu(true)
 
-        mBinding.etPhotoUrl.addTextChangedListener {
-            Glide.with(this)
-                .load(mBinding.etPhotoUrl.text.toString())
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .centerCrop()
-                .into(mBinding.imgPhoto)
+        setupTextFields()
+    }
+
+    private fun setupTextFields() {
+        with(mBinding) {
+            etName.addTextChangedListener { validateFields(tilName) }
+            etPhone.addTextChangedListener { validateFields(tilPhone) }
+            etPhotoUrl.addTextChangedListener {
+                validateFields( tilPhotoUrl)
+                loadImage(it.toString().trim())
+            }
         }
-        mBinding.etName.addTextChangedListener { validateFields(mBinding.tilName) }
-        mBinding.etPhone.addTextChangedListener { validateFields(mBinding.tilPhone) }
-        mBinding.etPhotoUrl.addTextChangedListener { validateFields(mBinding.tilPhotoUrl) }
+    }
+
+    private fun loadImage(url: String){
+        Glide.with(this)
+            .load(url)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .centerCrop()
+            .into(mBinding.imgPhoto)
     }
 
     private fun getStore(id: Long) {
