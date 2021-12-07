35a7a2c73cd82261375694e7d8a4993c6f8670b4
Josue <sjosue5082002@gmail.com>
2021-12-06 18:40:40 -0600

    Validar múltiples TextFields

--- app/src/main/java/com/example/stores_da/EditStoreFragment.kt
+++ app/src/main/java/com/example/stores_da/EditStoreFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.text.Editable
 import android.view.*
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -12,6 +13,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.stores_da.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
@@ -87,7 +89,8 @@ class EditStoreFragment : Fragment() {
                 true
             }
             R.id.action_save -> {
-                if (mStoreEntity != null && validateFields()){
+                if (mStoreEntity != null &&
+                    validateFields(mBinding.tilPhotoUrl,mBinding.tilPhone,mBinding.tilName)){
                     /*val store = StoreEntity(name = mBinding.etName.text.toString().trim(),
                     phone = mBinding.etPhone.text.toString().trim(),
                     website = mBinding.etWebsite.text.toString().trim(),
@@ -126,6 +129,23 @@ class EditStoreFragment : Fragment() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun validateFields(vararg textFields: TextInputLayout): Boolean{
+        var isValid = true
+
+        for (textField in textFields){
+            if (textField.editText?.text.toString().trim().isEmpty()){
+                textField.error = getString(R.string.helper_required)
+                textField.editText?.requestFocus()
+                isValid = false
+            }
+        }
+        if (!isValid) Snackbar.make(mBinding.root,
+            R.string.edit_store_message_valid,
+            Snackbar.LENGTH_SHORT).show()
+
+        return isValid
     }
 
     private fun validateFields(): Boolean {
